e3af9eea52a710275839ba62e14fa99c90b4fd3c
sahar-amjad1 <150724877+sahar-amjad1@users.noreply.github.com>
2024-01-12 17:13:16 +0500

    Add files via upload

---
+++ game/numberguessinggame.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+async function startGame() {
+    const secretNum = Math.floor(Math.random() * 10 + 1);
+    async function playRound() {
+        const userInput = await inquirer.prompt({
+            type: "number",
+            name: "userNum",
+            message: "Enter Your Number"
+        });
+        const userNum = userInput.userNum;
+        if (userNum === secretNum) {
+            console.log(`congratulations! Your guessed number is correct`);
+            return true;
+        }
+        else {
+            if (userNum > secretNum) {
+                console.log(`\n Your guessed number is greater than actual number`);
+            }
+            else if (userNum < secretNum) {
+                console.log(`\n Your guessed number is less than actual number`);
+            }
+            return false;
+        }
+    }
+    let isguessed = false;
+    let attempts = 0;
+    console.log(`\n Number Guessing Game!`);
+    while (!isguessed) {
+        attempts++;
+        console.log(`Your attempt No.${attempts}`);
+        isguessed = await playRound();
+    }
+    console.log(`You have attempted ${attempts} times to guessed the right NUmber`);
+}
+startGame();
